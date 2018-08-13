7bb0c55497e394dd4c0b787ab428a71fb8889bf3
Mendy <mendybeltcohen@gmail.com>
2018-08-13 01:34:30 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,13 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
+  else
+    count = 1
+    line = "The line is currently:"
+     katz_deli.each do |x|
+     line.push("#{count}.")
+     count+=1
   end
+  puts line
 end
   
